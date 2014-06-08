1d0a88466040d6111b7808a622d0133dd28aba99
SeanDavies <seandaviesopi@gmail.com>
2014-06-08 11:49:54 +1000

    Revert "Initial UE4 Commit"
    This reverts commit 02ee054e2680d67cbf499473314ad6cfe345456f.

--- Source/Azuruk/Azuruk.cpp
+++
@@ -1,5 +0,0 @@
-#include "Azuruk.h"
-
-#include "Azuruk.generated.inl"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Azuruk, "Azuruk" );
--- Source/Azuruk/Azuruk.h
+++
@@ -1,9 +0,0 @@
-#ifndef __AZURUK_H__
-#define __AZURUK_H__
-
-#pragma once
-
-#include "Engine.h"
-
-#endif
-
