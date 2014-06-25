fb7aa3349c360ab7f17a0e9a9164937074c3feb5
SeanDavies <seandaviesopi@gmail.com>
2014-06-25 12:22:26 +1000

    Health, Death and Ragdolling

--- Source/Azuruk/Character/AzurukBaseCharacter.cpp
+++ Source/Azuruk/Character/AzurukBaseCharacter.cpp
@@ -21,6 +21,124 @@ AAzurukBaseCharacter::AAzurukBaseCharacter(const class FPostConstructInitializeP
 	CharacterMovement->AirControl = 0.2f;
 }
 
+void AAzurukBaseCharacter::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+
+	if (Role = ROLE_Authority)
+	{
+		Health = GetMaxHealth();
+	}
+}
+
+float AAzurukBaseCharacter::TakeDamage(float Damage, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, class AActor* DamageCauser)
+{
+	if (!ShouldTakeDamage(Damage, DamageEvent, EventInstigator, DamageCauser))
+	{
+		return 0.f;
+	}
+
+	const float ActualDamage = Super::TakeDamage(Damage, DamageEvent, EventInstigator, DamageCauser);
+
+	if (ActualDamage > 0.f)
+	{
+		ModifyHealth(ActualDamage);
+
+		if (GetHealth() <= 0)
+		{
+			Die(ActualDamage, DamageEvent, EventInstigator, DamageCauser);
+		}
+		else
+		{
+			// Hit
+		}
+		// Cause a Noise
+		MakeNoise(1.0f, EventInstigator ? EventInstigator->GetPawn() : this);
+	}
+
+	return Damage;
+}
+
+bool AAzurukBaseCharacter::CanDie(float KillingDamage, FDamageEvent const& DamageEvent, AController* Killer, AActor* DamageCauser) const
+{
+	if (bIsDying										// already dying
+		|| IsPendingKill()								// already destroyed
+		|| Role != ROLE_Authority						// not authority
+		|| GetWorld()->GetAuthGameMode() == NULL
+		|| GetWorld()->GetAuthGameMode()->GetMatchState() == MatchState::LeavingMap)	// level transition occurring
+	{
+		return false;
+	}
+
+	return true;
+}
+
+void AAzurukBaseCharacter::Die(float KillingDamage, struct FDamageEvent const& DamageEvent, class AController* Killer, class AActor* DamageCauser)
+{
+	if (CanDie(KillingDamage, DamageEvent, Killer, DamageCauser))
+	{
+		if (bIsDying)
+		{
+			return;
+		}
+
+		bReplicateMovement = false;
+		bTearOff = true;
+		bIsDying = true;
+
+		DetachFromControllerPendingDestroy();
+
+		// disable collisions on capsule
+		CapsuleComponent->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+		CapsuleComponent->SetCollisionResponseToAllChannels(ECR_Ignore);
+
+		if (Mesh)
+		{
+			static FName CollisionProfileName(TEXT("Ragdoll"));
+			Mesh->SetCollisionProfileName(CollisionProfileName);
+		}
+		SetActorEnableCollision(true);
+
+		SetRagdollPhysics();
+	}
+}
+
+void AAzurukBaseCharacter::SetRagdollPhysics()
+{
+	if (Mesh || Mesh->GetPhysicsAsset())
+	{
+		// initialize physics/etc
+		Mesh->SetAllBodiesSimulatePhysics(true);
+		Mesh->SetSimulatePhysics(true);
+		Mesh->WakeAllRigidBodies();
+		Mesh->bBlendPhysics = true;
+
+		CharacterMovement->StopMovementImmediately();
+		CharacterMovement->DisableMovement();
+		CharacterMovement->SetComponentTickEnabled(false);
+
+		bInRagdoll = true;
+	}	
+}
+
+void AAzurukBaseCharacter::ModifyHealth(float Amount)
+{
+	Health = FMath::IsNegativeFloat(Amount) 
+		// Heal 
+		? FMath::Min(Health - Amount, GetMaxHealth())
+		// Damage
+		: FMath::Min(Health - Amount, 0.f);
+}
+
+float AAzurukBaseCharacter::GetHealth()
+{
+	return Health;
+}
+
+float AAzurukBaseCharacter::GetMaxHealth() const
+{
+	return GetClass()->GetDefaultObject<AAzurukBaseCharacter>()->Health;
+}
 
 
 
--- Source/Azuruk/Character/AzurukBaseCharacter.h
+++ Source/Azuruk/Character/AzurukBaseCharacter.h
@@ -13,5 +13,58 @@ UCLASS()
 class AAzurukBaseCharacter : public ACharacter
 {
 	GENERATED_UCLASS_BODY()
+
+public:
+
+	/* PostInitializeComponents */
+	virtual void PostInitializeComponents() OVERRIDE;
+
+//////////////////////////////////////////////////////////////////////////
+// Damage and Death
 	
+	/* Take damage, handle death */
+	virtual float TakeDamage(float Damage, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, class AActor* DamageCauser) OVERRIDE;
+
+	/** Returns True if the pawn can die in the current state */
+	virtual bool CanDie(float KillingDamage, FDamageEvent const& DamageEvent, AController* Killer, AActor* DamageCauser) const;
+
+	/*
+	* Kills pawn.  Server/authority only.
+	* @param KillingDamage - Damage amount of the killing blow
+	* @param DamageEvent - Damage event of the killing blow
+	* @param Killer - Who killed this pawn
+	* @param DamageCauser - the Actor that directly caused the damage (i.e. the Projectile that exploded, the Weapon that fired, etc)
+	* @returns true if allowed
+	*/
+	virtual void Die(float KillingDamage, struct FDamageEvent const& DamageEvent, class AController* Killer, class AActor* DamageCauser);
+
+	/** Identifies if pawn is in its dying state */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Health)
+		uint32 bIsDying : 1;
+
+private:
+
+	/* switch to ragdoll */
+	void SetRagdollPhysics();
+
+//////////////////////////////////////////////////////////////////////////
+// Health
+public:
+
+	/* Returns Health Value */
+	float GetHealth();
+
+	/* Returns Max Health */
+	float GetMaxHealth() const;
+
+private:
+
+	/* Modify Pawns Health 
+	 * Use Negative Input to Heal
+	 */
+	void ModifyHealth(float Amount);
+
+	/* Health Value */
+	float Health;
+
 };
